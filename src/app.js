33cd6e4928450006394709732f6327ea33d28e1e
abidmi <abelragazzo@gmail.com>
2023-07-12 14:13:00 +0700

    coba commit untuk cicd
    Signed-off-by: abidmi <abelragazzo@gmail.com>

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 
 app.get("/", (_req, res) =>  {
-  res.status(200).send("Hello world")
+  res.status(200).send("Hello worlds")
 })
 module.exports = app;
