6f22ece268422682333121d9fb894a5eba560368
devsintek <118410225+devsintek@users.noreply.github.com>
2023-07-12 11:04:45 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('private.key'),
+  cert: fs.readFileSync('certificate.crt')
+};
+
+const server = https.createServer(options, (req, res) => {
+  res.writeHead(200);
+  res.end('Hi Beb...');
+});
+
+server.listen(62303, () => {
+  console.log('HTTPS server listening on port ...');
+});
