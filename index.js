6d52bc955c28c47f5535f569d9bbf5973d9b5cd5
devsintek <118410225+devsintek@users.noreply.github.com>
2023-07-13 13:03:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./server')
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 62303
 server.listen(PORT, () => {
     console.log(`This app running on http://localhost:${PORT}`)
 })
