0e1245c344b3a3c7416b68e8a8382b9f5dfc237b
abidmi <abelragazzo@gmail.com>
2023-07-14 11:31:56 +0700

    coba berubah

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const options = {
 
 const server = https.createServer(options, (req, res) => {
   res.writeHead(200);
-  res.end('Hi beb...');
+  res.end('Hai beb...');
 });
 
 server.listen(62303, () => {
